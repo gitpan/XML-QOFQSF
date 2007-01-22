f34bbfd29d78c24906362cd092d8f379abe96a7a
Neil Williams <codehelp@debian.org>
2007-01-22 23:45:28 +0000

    Import of CODEHELP/XML-QOFQSF-0.02 from CPAN.
    gitpan-cpan-distribution: XML-QOFQSF
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         CODEHELP/XML-QOFQSF-0.02.tar.gz
    gitpan-cpan-author:       CODEHELP
    gitpan-cpan-maturity:     released

--- lib/XML/QOFQSF.pm
+++ lib/XML/QOFQSF.pm
@@ -530,11 +530,11 @@ Support for the QOF SQLite backend will be added in due course.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
